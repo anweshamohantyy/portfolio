e08ea3d31f030b064f1a0d093d1f86eea37bea6c
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-04-17 20:26:51 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,10 +4,48 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/anweshamohantyy", title: "GitHub" }
+];
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = p.title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
