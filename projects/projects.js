5206ce47bceb5e0387fb5cc9ca299e16df59e6d0
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-04-24 22:27:26 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
+const title = document.querySelector('.projects-title');
+title.textContent = `${projects.length} Projects`;
 renderProjects(projects, projectsContainer, 'h2');
