c89d2424f7adf78a97672a275bba9aff9adf5b25
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-04-24 22:42:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
+const githubData = await fetchGitHubData('anweshamohantyy');
