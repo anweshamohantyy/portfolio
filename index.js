7b38cf3fafbad12201f929f9f7f2e90ce68506bf
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-04-24 22:44:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
 const githubData = await fetchGitHubData('anweshamohantyy');
+const profileStats = document.querySelector('#profile-stats');
+
+profileStats.innerHTML = `
+  <dl>
+    <dt>Public repos</dt><dd>${profile.public_repos}</dd>
+    <dt>Public gists</dt><dd>${profile.public_gists}</dd>
+    <dt>Followers</dt><dd>${profile.followers}</dd>
+    <dt>Following</dt><dd>${profile.following}</dd>
+  </dl>
+`;
