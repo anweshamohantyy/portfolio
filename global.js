db58c2937d8746ab0ee665b308fa809ad1b07a05
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-04-24 23:48:03 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,14 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// let navLinks = $$("nav a");
-
-// let currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname
-// );
-
-// currentLink?.classList.add('current');
-
 let pages = [
   { url: "", title: "Home" },
   { url: "projects/", title: "Projects" },
@@ -67,8 +59,6 @@ document.body.insertAdjacentHTML(
 let select = document.querySelector("select");
 
 select.addEventListener("input", function (event) {
-  console.log("color scheme changed to", event.target.value);
-
   document.documentElement.style.setProperty(
     "color-scheme",
     event.target.value
@@ -78,7 +68,6 @@ select.addEventListener("input", function (event) {
 
 if ("colorScheme" in localStorage) {
   let saved = localStorage.colorScheme;
-
   document.documentElement.style.setProperty("color-scheme", saved);
   select.value = saved;
 }
@@ -89,7 +78,6 @@ form?.addEventListener("submit", function (event) {
   event.preventDefault();
 
   let data = new FormData(form);
-
   let params = [];
 
   for (let [name, value] of data) {
@@ -97,17 +85,17 @@ form?.addEventListener("submit", function (event) {
   }
 
   let url = form.action + "?" + params.join("&");
-
   location.href = url;
 });
 
 export async function fetchJSON(url) {
   try {
-    // Fetch the JSON file from the given URL
     const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
+
     const data = await response.json();
     return data;
   } catch (error) {
@@ -115,16 +103,19 @@ export async function fetchJSON(url) {
   }
 }
 
-
 export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
+
   for (let project of projects) {
     const article = document.createElement('article');
+
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
+      <p>Year: ${project.year}</p>
       <p>${project.description}</p>
     `;
+
     containerElement.appendChild(article);
   }
 }
@@ -132,14 +123,3 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 export async function fetchGitHubData(username) {
   return fetchJSON(`https://api.github.com/users/${username}`);
 }
-
-article.innerHTML = `
-  <${headingLevel}>${project.title}</${headingLevel}>
-  <img src="${project.image}" alt="${project.title}">
-  <p>Year: ${project.year}</p>
-  <p>${project.description}</p>
-`;
-
-
-
-
