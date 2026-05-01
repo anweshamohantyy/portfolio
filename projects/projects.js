b5902046c89a4f0ac4ef8995fe68ac396ea9a55f
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-05-01 13:56:56 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -4,3 +4,14 @@ const projectsContainer = document.querySelector('.projects');
 const title = document.querySelector('.projects-title');
 title.textContent = `${projects.length} Projects`;
 renderProjects(projects, projectsContainer, 'h2');
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
