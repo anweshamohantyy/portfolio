ee4a61881576a98fd5109647dff1e10410a0b429
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-05-08 20:01:37 -0700

    Update global.js

--- global.js
+++ global.js
@@ -8,6 +8,7 @@ let pages = [
   { url: "", title: "Home" },
   { url: "projects/", title: "Projects" },
   { url: "contact/", title: "Contact" },
+  { url: "meta/", title: "Meta" },
   { url: "resume/", title: "Resume" },
   { url: "https://github.com/anweshamohantyy", title: "GitHub" }
 ];
