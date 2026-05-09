6a132c2826fc9796a5d55ef5525e2d323274e19a
Anwesha Mohanty <anwesha.mohantyy@gmail.com>
2026-05-08 17:29:52 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -12,8 +12,6 @@ async function loadData() {
   return data;
 }
 
-let data = await loadData();
-
 function processCommits(data) {
   return d3
     .groups(data, (d) => d.commit)
@@ -31,51 +29,105 @@ function processCommits(data) {
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
         totalLines: lines.length,
       };
-
       Object.defineProperty(ret, 'lines', {
         value: lines,
         enumerable: false,
         writable: true,
         configurable: true,
       });
-
       return ret;
     });
 }
 
-let data = await loadData();
-let commits = processCommits(data);
-console.log(commits);
-
 function renderCommitInfo(data, commits) {
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
-  // Total LOC
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
 
-  // Total commits
   dl.append('dt').text('Total commits');
   dl.append('dd').text(commits.length);
 
-  // Number of files
   dl.append('dt').text('Number of files');
   dl.append('dd').text(d3.group(data, d => d.file).size);
 
-  // Maximum depth
   dl.append('dt').text('Max depth');
   dl.append('dd').text(d3.max(data, d => d.depth));
 
-  // Longest line
   dl.append('dt').text('Longest line (chars)');
   dl.append('dd').text(d3.max(data, d => d.length));
 
-  // Average file length
   const fileLengths = d3.rollups(data, v => d3.max(v, v => v.line), d => d.file);
   dl.append('dt').text('Avg file length');
   dl.append('dd').text(Math.round(d3.mean(fileLengths, d => d[1])));
 }
 
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([usableArea.bottom, usableArea.top]);
+
+  const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+  gridlines.call(
+    d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width)
+  );
+
+  const dots = svg.append('g').attr('class', 'dots');
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+
+  const xAxis = d3.axisBottom(xScale);
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+}
+
 let data = await loadData();
 let commits = processCommits(data);
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
